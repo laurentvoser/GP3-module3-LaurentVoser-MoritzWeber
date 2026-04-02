a1527f6d0c0daf1f6177f886644b15e7b1c9de44
Laurent Voser <laurent.voser@students.unibe.ch>
2026-04-02 15:02:53 +0200

    es funktioniiirt

---
+++ analysis/01.1_Setup_Data.R
@@ -0,0 +1,27 @@
+
+
+## MODIS Phenology-Daten laden
+phenology <- mt_subset(
+  product = "MCD12Q2",
+  lat = lat,
+  lon = lon,
+  band = c("Greenup.Num_Modes_01","Maturity.Num_Modes_01"),
+  start = "2001-01-01",
+  end   = "2010-12-31",
+  km_lr = 100, km_ab = 100,
+  site_name = "adirondacks",
+  internal = TRUE, progress = FALSE
+)
+write.csv(phenology, "data-raw/phenology.csv", row.names = FALSE)
+
+## Land Cover-Daten laden
+land_cover <- mt_subset(
+  product = "MCD12Q1",
+  lat = lat, lon = lon,
+  band = "LC_Type1",
+  start = "2010-01-01", end = "2010-12-31",
+  km_lr = 100, km_ab = 100,
+  site_name = "adirondacks_lc",
+  internal = TRUE
+)
+write.csv(land_cover, "data-raw/land_cover.csv", row.names = FALSE)
