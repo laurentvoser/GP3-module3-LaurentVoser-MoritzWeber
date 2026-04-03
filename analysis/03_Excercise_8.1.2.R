6a1f26b2278ad0eb9048a2629340c6c8c44476c6
bing-bongdingdong <moritzbrunoweber@gmail.com>
2026-04-03 12:37:06 +0200

    vvv

--- analysis/03_Excercise_8.1.2.R
+++ analysis/03_Excercise_8.1.2.R
@@ -4,24 +4,6 @@ library(tidyverse)
 library(geodata)
 library(tmap)
 
-# --- Parameter ---
-lat <- 43.5
-lon <- -74.5
-years <- 2001:2010
-
-# --- MODIS Phenology-Daten laden ---
-phenology <- mt_subset(
-  product = "MCD12Q2",
-  lat = lat,
-  lon = lon,
-  band = c("Greenup.Num_Modes_01","Maturity.Num_Modes_01"),
-  start = "2001-01-01",
-  end   = "2010-12-31",
-  km_lr = 100, km_ab = 100,
-  site_name = "adirondacks",
-  internal = TRUE, progress = FALSE
-)
-
 # Aufbereiten
 
 phenology <- phenology %>%
